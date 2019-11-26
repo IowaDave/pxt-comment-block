9400bff74ff39a7f8013cb6fbc1962243573ef09
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-11-25 19:01:37 -0600

    Updates.

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@
 namespace comment {
     /**
      * Write a comment
-     * @param theComment eg:write|comment|here
+     * @param theComment eg:'write comment here'
      */
     //% block=comment|%theComment
     export function comment(theComment: string): void {
