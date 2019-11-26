cf15083ea8bbe83e40ac3ed2af02767fe2d34da6
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-11-25 19:04:05 -0600

    Updates.

--- test.ts
+++ test.ts
@@ -1,3 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-comment.comment("write|comment|here")
+comment.comment("write comment here")
+
+comment.comment("A visible user comment")
